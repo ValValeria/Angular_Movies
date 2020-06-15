acb88b18bdb12887a410423eaa5810a2cb35820f
Vasiliy Pupkin <led2017l@gmail.com>
2020-06-15 15:27:43 +0300

    Make it better

--- main_server/server/models.py
+++ main_server/server/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 # Create your models here.
- class Posts(models.Model):
+class Users(models.Model):
+     email=models.EmailField()
+     name=models.CharField(max_length=20)
+     password=models.CharField(max_length=10)
+
+class Posts(models.Model):
      p1=models.CharField(max_length=300)
      title=models.CharField(max_length=30)
-     userId=models.ForeignKey(User,on_delete=models.CASCADE)
+     userId=models.ForeignKey(Users,on_delete=models.CASCADE)
 
- class Users(models.Model):
-     email=models.EmailField()
-     name=models.CharFields()
-     password=models.CharField()
 
      
